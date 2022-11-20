4837705237efb35b28475b328ca15d42180474fd
Jamshaid khan <jkniazi302@gmail.com>
2022-11-20 18:21:40 +0500

    remove test setup

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ require("dotenv").config();
 
 app.get('/',(req,res)=>{
     res.send('Deploy Project Successfully');
-})
+});
 
 const user = require("./controller/users/user");
 app.use('/user',user);
@@ -28,9 +28,9 @@ app.use('/api/product',product);
 const category = require("./controller/products/category");
 app.use('/api/category',category);
 
-//server
+
 const port = process.env.PORT || 2022;
 app.listen(port,function () {
     console.log("Listening at port",port);
 });
-module.exports = app
+module.exports = app;
